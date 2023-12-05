f8832b2df090fd60f9b53bc528a0b72fe3ce6c0c
Truitt Thuecks <truittthuecks@truitts-air.lan>
2023-12-05 14:28:08 -0500

    Initial code push

---
+++ Wages.cs/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Wages.cs;
+
+class Wages
+{
+    static void Main(string[] args)
+    {
+
+        string name = "truitt";
+        double regularPay = 18.00;
+        double overtimePay = 20.00;
+        double overtimeHours = 45.50;
+        double regularHours = 39.00;
+        double totalWages = 1602.00;
+
+        Console.WriteLine("Enter your name: " + name);
+        Console.WriteLine("Enter your regular pay: " + regularPay);
+
+        Console.WriteLine("Enter your overtime pay: " + overtimePay);
+        Console.WriteLine("Enter your overtime hours: " + overtimeHours);
+        Console.WriteLine("Enter your regular hours: " + regularHours);
+        Console.WriteLine("Enter your total wages: " + totalWages);
+    }
+}
---
+++ Wages.cs/Project.cs
@@ -0,0 +1,24 @@
+﻿namespace Project.cs;
+
+ class Program
+{
+    static void Main(string[] args)
+    {
+
+        string name = "truitt";
+        double regularPay = 18.00;
+        double overtimePay = 20.00;
+        double overtimeHours = 45.50;
+        double regularHours = 39.00;
+        double totalWages = 1602.00;
+
+        Console.WriteLine("Enter your name: "  + name);
+        Console.WriteLine("Enter your regular pay: " + regularPay);
+    
+        Console.WriteLine("Enter your overtime pay: " + overtimePay);
+        Console.WriteLine("Enter your overtime hours: " + overtimeHours);
+        Console.WriteLine("Enter your regular hours: " + regularHours);
+        Console.WriteLine("Enter your totalWages: " + totalWages);
+    }
+}
+
